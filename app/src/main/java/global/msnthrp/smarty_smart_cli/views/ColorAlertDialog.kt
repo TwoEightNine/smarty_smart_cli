7993fa5e86724deb7bc849e5e794e1ddeecc092a
twoeightnine <twoeightnine@list.ru>
2018-11-14 17:14:33 +0400

    add 'turn off' to rgb, improve ux

--- app/src/main/java/global/msnthrp/smarty_smart_cli/views/ColorAlertDialog.kt
+++ app/src/main/java/global/msnthrp/smarty_smart_cli/views/ColorAlertDialog.kt
@@ -1,8 +1,10 @@
 package global.msnthrp.smarty_smart_cli.views
 
 import android.content.Context
+import android.graphics.Color
 import android.support.v7.app.AlertDialog
 import android.view.View
+import android.widget.TextView
 import com.flask.colorpicker.ColorPickerView
 import global.msnthrp.smarty_smart_cli.R
 import global.msnthrp.smarty_smart_cli.extensions.view
@@ -15,6 +17,7 @@ class ColorAlertDialog(context: Context,
                        private val onColorChanged: (String) -> Unit) : AlertDialog(context) {
 
     private val colorPicker: ColorPickerView by view(R.id.colorPicker)
+    private val tvBlack: TextView by view(R.id.tvBlack)
 
     private var disposable: Disposable? = null
 
@@ -23,17 +26,23 @@ class ColorAlertDialog(context: Context,
         setView(view)
         view.post {
             with(colorPicker) {
-                setInitialColor(colorToInt(currentColor), false)
+                setInitialColor(currentColor)
                 addOnColorChangedListener(::onColorChanged)
             }
+            tvBlack.setOnClickListener { turnOff() }
         }
-
     }
 
     private fun onColorChanged(color: Int) {
         disposable?.dispose()
         disposable = Observable.timer(DELAY, TimeUnit.MILLISECONDS)
                 .subscribe { onColorChanged.invoke(colorToStr(color)) }
+    }
+
+    private fun turnOff() {
+        disposable?.dispose()
+        onColorChanged.invoke(colorToStr(Color.BLACK))
+        dismiss()
     }
 
     override fun dismiss() {
@@ -44,6 +53,12 @@ class ColorAlertDialog(context: Context,
     private fun colorToInt(color: String) = Integer.parseInt(color, 16) or (0xff shl 24)
 
     private fun colorToStr(color: Int) = Integer.toHexString(color).substring(2)
+
+    private fun ColorPickerView.setInitialColor(color: String) {
+        var intColor = colorToInt(color)
+        if (intColor == Color.BLACK) intColor = Color.WHITE
+        setInitialColor(intColor, false)
+    }
 
     companion object {
         const val DELAY = 200L
